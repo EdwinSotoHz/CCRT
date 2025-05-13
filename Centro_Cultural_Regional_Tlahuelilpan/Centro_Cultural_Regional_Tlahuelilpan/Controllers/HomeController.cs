0db1da440281716f06fa1b69aac8c239968d68ae
Edwin Salvador Soto Hernandez <230110358@itsoeh.edu.mx>
2025-05-13 12:48:01 -0600

    First Checkpoint

---
+++ Centro_Cultural_Regional_Tlahuelilpan/Centro_Cultural_Regional_Tlahuelilpan/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using Centro_Cultural_Regional_Tlahuelilpan.Models;
+using Centro_Cultural_Regional_Tlahuelilpan.Models.DBCRUDCORE;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Centro_Cultural_Regional_Tlahuelilpan.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly CentroCulturalRegionalTlahuelilpanContext _DBContext;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+
+        public HomeController(CentroCulturalRegionalTlahuelilpanContext context, IWebHostEnvironment hostingEnvironment, ILogger<HomeController> logger)
+        {
+            _DBContext = context;
+            _hostingEnvironment = hostingEnvironment;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            List<Tallere> list = _DBContext.Talleres.ToList();
+            return View(list);
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Centro_Cultural_Regional_Tlahuelilpan/Centro_Cultural_Regional_Tlahuelilpan/Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+using Microsoft.AspNetCore.Mvc;
+using Centro_Cultural_Regional_Tlahuelilpan.Models.DBCRUDCORE;
+using Centro_Cultural_Regional_Tlahuelilpan.Models.ViewModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Centro_Cultural_Regional_Tlahuelilpan.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly CentroCulturalRegionalTlahuelilpanContext _DBContext;
+
+        public StudentsController(CentroCulturalRegionalTlahuelilpanContext context)
+        {
+            _DBContext = context;
+        }
+
+        public IActionResult Students()
+        {
+            var students = _DBContext.Alumnos.Include(a => a.Expediente).Include(a => a.ProgresoEstudiantils).ToList();
+            return View(students);
+        }
+    }
+}
